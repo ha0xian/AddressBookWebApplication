42dc9b458bad8e2b49e97c61a36a223ae8192713
HaoXian <chunghaoxian@gmail.com>
2023-01-01 16:16:04 +0000

    Added Model

---
+++ AddressBook/Address Book/Models/AddressBookViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Address_Book.Models
+{
+    public class AddressBookViewModel
+    {
+        public int Id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+    }
+}
