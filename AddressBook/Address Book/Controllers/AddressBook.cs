c1b93e1c15531fd183c1053181b6e4ba7b862c85
HaoXian <chunghaoxian@gmail.com>
2023-01-01 16:10:04 +0000

    Added AddressBookController

---
+++ AddressBook/Address Book/Controllers/AddressBook.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Address_Book.Controllers
+{
+    public class AddressBook : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
