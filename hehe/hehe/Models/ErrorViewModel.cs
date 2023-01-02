da3f0ac42a4c95ae52ac3d52dea169856ad77abd
ha0xian <109151618+ha0xian@users.noreply.github.com>
2023-01-02 03:43:48 +0000

    Delete hehe directory

--- hehe/hehe/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace hehe.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
